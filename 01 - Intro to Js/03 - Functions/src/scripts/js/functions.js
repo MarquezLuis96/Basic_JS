6032e4aa2abdfc7d4b182801fd40dffcb279062e
MarquezLuis96 <englamc@gmail.com>
2021-08-21 18:59:06 -0400

    Learning about functions

---
+++ 01 - Intro to Js/03 - Functions/src/scripts/js/functions.js
@@ -0,0 +1,12 @@
+//Call functions with console
+
+
+//DECLARATIVE
+    function sayHelloTo(helloTo) {
+        return "Hello " + helloTo + " welcome to this awesome website";
+    }
+
+//EXPRESSION
+    var wavingTo = function(helloTo) {
+        return "Hello " + helloTo + "✋🏻🖖🏻🤘🏻🤙🏻 Welcome to this awesome website";
+    }
