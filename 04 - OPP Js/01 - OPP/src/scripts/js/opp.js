58eb8e01540ff9e34b85652959a74e435f678e45
MarquezLuis96 <englamc@gmail.com>
2021-08-27 16:42:53 -0400

    Learning about OOP with Js

--- 04 - OPP Js/01 - OPP/src/scripts/js/opp.js
+++ 04 - OPP Js/01 - OPP/src/scripts/js/opp.js
@@ -1,1 +1,9 @@
-window.alert("Hello, this is a test");
+var myCar = {
+    brand : "Jeep",
+    model : "Grand Cherokee",
+    year : "2021",
+
+    details: function() {
+        console.log("Brand:\t" + this.brand + "\nModel:\t" + this.model + "\nYear:\t" + this.year);
+    }
+};
