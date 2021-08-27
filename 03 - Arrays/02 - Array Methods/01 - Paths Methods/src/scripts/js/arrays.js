7c334d16122aa2df522d9c329cef80d79ae5f74e
MarquezLuis96 <englamc@gmail.com>
2021-08-27 17:44:42 -0400

    Learnig about filter function

---
+++ 03 - Arrays/02 - Array Methods/01 - Paths Methods/src/scripts/js/arrays.js
@@ -0,0 +1,20 @@
+var articles = [
+    {articleName: "Bike", articleCost: 1500},
+    {articleName: "TV", articleCost: 250},
+    {articleName: "Book", articleCost: 150},
+    {articleName: "Redmi Note 7 pro", articleCost: 280},
+    {articleName: "Laptop", articleCost: 850},
+    {articleName: "Keyboard", articleCost: 50},
+    {articleName: "Headphones", articleCost: 100},
+];
+
+var filtered = articles.filter(function(article) {
+    return article.articleCost <= 500;
+});
+
+
+function filterByArgument(arg) {
+    return articles.filter(function(article) {
+        return article.articleCost <= arg;
+    });
+}
